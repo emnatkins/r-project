2412f488ddc44ba79e688112d56fa961a1f325b4
nobody <nobody@00db46b3-68df-0310-9c12-caf00c1e9a41>
1998-11-13 14:37:19 +0000

    This commit was manufactured by cvs2svn to create tag 'R-0-63'.

---
+++ tags/R-0-63/src/include/S_compat.h
@@ -0,0 +1,33 @@
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* S compatibility library headers --
+			maps internal functions in S to R equivalents
+ */
+
+#ifndef S_COMPAT_H_
+#define S_COMPAT_H_
+
+#include "S.h"
+#include "Linpack.h"/*-> Blas.h */
+
+extern void
+F77_SYMBOL(dqrdca) (double*, long int*, long int*, long int*,
+		    double*, long int*, double*, long int*, double*);
+#endif
---
+++ tags/R-0-63/src/unix/devX11.h
@@ -0,0 +1,9 @@
+/* Declarations (for devX11.c) which are not only there,
+ * ------------ but also, e.g., in  system.c  [event loop]
+ */
+int X11ConnectionNumber();
+void ProcessEvents(void);
+int X11DeviceDriver(DevDesc*, char*, double, double, double);
+#ifdef SOON
+int XFigDeviceDriver(char**, int, double*, int);
+#endif
