83c20d7d38a743d3d79c8a901680e049163ca64a
nobody <nobody@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-04-14 08:44:18 +0000

    This commit was manufactured by cvs2svn to create tag 'R-1-0-1'.

---
+++ tags/R-1-0-1/src/library/base/R/text.R
@@ -0,0 +1,28 @@
+as.char.or.expr <- function(x) {
+    if (is.expression(x)) x 
+    else if (is.call(x)) as.expression(x)
+    else as.character(x)
+}
+
+text <- function(x, ...) UseMethod("text")
+text.default <- function(x, y = NULL, labels = seq(along = x),
+                         adj = NULL, pos = NULL, offset = 0.5, 
+                         vfont = NULL, cex = 1, col = NULL,
+			 font = NULL, xpd = NA, ...) {
+    if (!missing(y) && (is.character(y) || is.expression(y))) {
+	labels <- y; y <- NULL
+    }
+    if (!is.null(vfont)) {
+        typeface <- pmatch(vfont[1], c("serif", "sans serif", "script",
+		                       "gothic english", "gothic german",
+			      	       "gothic italian", "serif symbol",
+				       "sans serif symbol"))
+        fontindex <- pmatch(vfont[2], c("symbol", "plain", "italic", "bold",
+				        "bold italic", "cyrillic",
+					"oblique cyrillic", "EUC"))
+        vfont <- c(typeface-1, fontindex-1)
+    }
+    .Internal(text(xy.coords(x,y, recycle=TRUE),
+		   as.char.or.expr(labels), adj, pos, offset, vfont,
+		   cex, col, font, xpd, ...))
+}
