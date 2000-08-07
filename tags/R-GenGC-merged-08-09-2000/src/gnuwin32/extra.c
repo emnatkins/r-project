6981edddd698d47b6c27edbec116a7d49da1ef60
nobody <nobody@00db46b3-68df-0310-9c12-caf00c1e9a41>
2000-08-07 02:05:34 +0000

    This commit was manufactured by cvs2svn to create tag 'R-GenGC-merged-08-09-2000'.

---
+++ tags/R-GenGC-merged-08-09-2000/src/gnuwin32/extra.c
@@ -0,0 +1,562 @@
+/*
+ *  R : A Computer Language for Statistical Data Analysis
+ *  file extra.c
+ *  Copyright (C) 1998--2000  Guido Masarotto and Brian Ripley
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+/* extra commands for R */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include "Defn.h"
+#include "Fileio.h"
+#include <io.h>
+#include <direct.h>
+#include <time.h>
+#include <windows.h>
+#include "graphapp/ga.h"
+#include "rui.h"
+
+char * Rwin32_tmpnam(char * prefix)
+{
+    char *tmp, tm[MAX_PATH], tmp1[MAX_PATH], *p, *res;
+    int hasspace = 0;
+    unsigned int n, done = 0;
+    WIN32_FIND_DATA fd;
+    HANDLE h;
+
+    tmp = getenv("TMP");
+    if (!tmp) tmp = getenv("TEMP");
+    if (!tmp) tmp = getenv("R_USER"); /* this one will succeed */
+    /* make sure no spaces in path */
+    for (p = tmp; *p; p++)
+	if (isspace(*p)) { hasspace = 1; break; }
+    if (hasspace)
+	GetShortPathName(tmp, tmp1, MAX_PATH);
+    else
+	strcpy(tmp1, tmp);
+    for (n = 0; n < 100; n++) {
+	/* try a random number at the end */
+        sprintf(tm, "%s\\%s%d", tmp1, prefix, rand());
+        if ((h = FindFirstFile(tm, &fd)) == INVALID_HANDLE_VALUE) {
+	    done = 1;
+	    break;
+	}
+        FindClose(h);
+        tm[0] = '\0';
+    }
+    if(!done)
+	error("cannot find unused tempfile name");
+    res = (char *) malloc((strlen(tm)+1) * sizeof(char));
+    strcpy(res, tm);
+    return res;
+}
+
+
+SEXP do_tempfile(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    SEXP  ans;
+    char *tn, *tm;
+    int i, slen=0 /* -Wall */;
+
+    checkArity(op, args);
+    if (!isString(CAR(args)) || (slen = LENGTH(CAR(args))) < 1)
+	errorcall(call, "invalid file name argument");
+    PROTECT(ans = allocVector(STRSXP, slen));
+    for(i = 0; i < slen; i++) {
+	tn = CHAR(STRING_ELT(CAR(args), i));
+	/* try to get a new file name */
+	tm = Rwin32_tmpnam(tn);
+	SET_STRING_ELT(ans, i, mkChar(tm));
+	free(tm);
+    }
+    UNPROTECT(1);
+    return (ans);
+}
+
+SEXP do_dircreate(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    SEXP  path, ans;
+    char *p, dir[MAX_PATH];
+    int res;
+
+    checkArity(op, args);
+    path = CAR(args);
+    if (!isString(path) || length(path) != 1)
+	errorcall(call, "invalid path argument");
+    strcpy(dir, CHAR(STRING_ELT(path, 0)));
+    for(p = dir; *p != '\0'; p++)
+	if(*p == '/') *p = '\\';
+    res = mkdir(dir);
+    PROTECT(ans = allocVector(LGLSXP, 1));
+    LOGICAL(ans)[0] = (res==0);
+    UNPROTECT(1);
+    return (ans);
+}
+
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+SEXP do_unlink(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    SEXP  fn, ans;
+    char *p, tmp[MAX_PATH], dir[MAX_PATH];
+    WIN32_FIND_DATA find_data;
+    HANDLE fh;
+    int i, nfiles, failures = 0;
+    struct stat sb;
+
+    checkArity(op, args);
+    fn = CAR(args);
+    nfiles = length(fn);
+    if (!isString(fn) || nfiles < 1)
+	errorcall(call, "invalid file name argument");
+    for(i = 0; i < nfiles; i++) {
+	strcpy(tmp, CHAR(STRING_ELT(fn, i)));
+	for(p = tmp; *p != '\0'; p++)
+	    if(*p == '/') *p = '\\';
+	if(stat(tmp, &sb) == 0)
+	    /* Is this a directory? */
+	    if(sb.st_mode & _S_IFDIR) {
+		if(rmdir(tmp)) failures++;
+		continue;
+	    }
+	/* Regular file (or more) */
+	strcpy(dir, tmp);
+	if ((p = strrchr(dir, '\\'))) *(++p) = '\0'; else *dir = '\0';
+	/* check for wildcard matches */
+	fh = FindFirstFile(tmp, &find_data);
+	if (fh != INVALID_HANDLE_VALUE) {
+	    strcpy(tmp, dir); strcat(tmp, find_data.cFileName);
+	    failures += (unlink(tmp) !=0);
+	    while(FindNextFile(fh, &find_data)) {
+		strcpy(tmp, dir); strcat(tmp, find_data.cFileName);
+		failures += (unlink(tmp) !=0);
+	    }
+	    FindClose(fh);
+	} /* else  failures++;*/
+    }
+    PROTECT(ans = allocVector(INTSXP, 1));
+    if (!failures)
+	INTEGER(ans)[0] = 0;
+    else
+	INTEGER(ans)[0] = 1;
+    UNPROTECT(1);
+    return (ans);
+}
+
+SEXP do_helpstart(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    char *home, buf[MAX_PATH];
+    FILE *ff;
+
+    checkArity(op, args);
+    home = getenv("R_HOME");
+    if (home == NULL)
+	error("R_HOME not set");
+    sprintf(buf, "%s\\doc\\html\\rwin.html", home);
+    ff = fopen(buf, "r");
+    if (!ff) {
+	sprintf(buf, "%s\\doc\\html\\rwin.htm", home);
+	ff = fopen(buf, "r");
+	if (!ff) {
+	    sprintf(buf, "%s\\doc\\html\\rwin.htm[l] not found", home);
+	    error(buf);
+	}
+    }
+    fclose(ff);
+    ShellExecute(NULL, "open", buf, NULL, home, SW_SHOW);
+    return R_NilValue;
+}
+
+static int nhfiles = 0;
+static char *hfiles[50];
+
+
+SEXP do_helpitem(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+/*
+ * type = 1: launch html file.
+ *        2: "topic", 2, Windows help file.
+ *        3: notify are finished with the help file.
+ */
+
+    char *item, *hfile;
+    char *home, buf[MAX_PATH];
+    FILE *ff;
+    int   type;
+
+    checkArity(op, args);
+    if (!isString(CAR(args)))
+	errorcall(call, "invalid topic argument");
+    item = CHAR(STRING_ELT(CAR(args), 0));
+    type = asInteger(CADR(args));
+    if (type == 1) {
+	ff = fopen(item, "r");
+	if (!ff) {
+	    sprintf(buf, "%s not found", item);
+	    error(buf);
+	}
+	fclose(ff);
+	home = getenv("R_HOME");
+	if (home == NULL)
+	    error("R_HOME not set");
+	ShellExecute(NULL, "open", item, NULL, home, SW_SHOW);
+    } else if (type == 2) {
+	if (!isString(CADDR(args)))
+	    errorcall(call, "invalid hlpfile argument");
+	hfile = CHAR(STRING_ELT(CADDR(args), 0));
+	if (!WinHelp((HWND) 0, hfile, HELP_KEY, (DWORD) item))
+	    warning("WinHelp call failed");
+	else {
+	    if (nhfiles >= 50)
+		error("too many .hlp files opened");
+	    hfiles[nhfiles] = malloc(strlen(hfile) * sizeof(char));
+	    strcpy(hfiles[nhfiles++], hfile);
+	}
+    } else if (type == 3) {
+	if (!isString(CADDR(args)))
+	    warningcall(call, "invalid hlpfile argument");
+	hfile = CHAR(STRING_ELT(CADDR(args), 0));
+	if (!WinHelp((HWND) 0, hfile, HELP_QUIT, (DWORD) 0))
+	    error("WinHelp call failed");
+    } else
+	warning("type not yet implemented");
+    return R_NilValue;
+}
+
+void closeAllHlpFiles()
+{
+    int   i;
+
+    for (i = nhfiles - 1; i >= 0; i--)
+	WinHelp((HWND) 0, hfiles[i], HELP_QUIT, (DWORD) 0);
+}
+
+
+SEXP do_flushconsole(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    R_FlushConsole();
+    return R_NilValue;
+}
+
+#include <winbase.h>
+/* typedef struct _OSVERSIONINFO{
+    DWORD dwOSVersionInfoSize;
+    DWORD dwMajorVersion;
+    DWORD dwMinorVersion;
+    DWORD dwBuildNumber;
+    DWORD dwPlatformId;
+    TCHAR szCSDVersion[ 128 ];
+    } OSVERSIONINFO; */
+
+
+SEXP do_winver(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    char isNT[8]="??", ver[256];
+    SEXP ans;
+    OSVERSIONINFO verinfo;
+
+    checkArity(op, args);
+    verinfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+    GetVersionEx(&verinfo);
+    switch(verinfo.dwPlatformId) {
+    case VER_PLATFORM_WIN32_NT:
+	strcpy(isNT, "NT");
+	break;
+    case VER_PLATFORM_WIN32_WINDOWS:
+	strcpy(isNT, "9x");
+	break;
+    case VER_PLATFORM_WIN32s:
+	strcpy(isNT, "win32s");
+	break;
+    default:
+	sprintf(isNT, "ID=%d", (int)verinfo.dwPlatformId);
+	break;
+    }
+
+    sprintf(ver, "Windows %s %d.%d (build %d) %s", isNT,
+	    (int)verinfo.dwMajorVersion, (int)verinfo.dwMinorVersion,
+	    LOWORD(verinfo.dwBuildNumber), verinfo.szCSDVersion);
+
+    PROTECT(ans = allocVector(STRSXP, 1));
+    SET_STRING_ELT(ans, 0, mkChar(ver));
+    UNPROTECT(1);
+    return (ans);
+}
+
+void internal_shellexec(char * file)
+{
+    char *home, buf[MAX_PATH];
+
+    home = getenv("R_HOME");
+    if (home == NULL)
+	error("R_HOME not set");
+    strcpy(buf, file);
+    ShellExecute(NULL, "open", buf, NULL, home, SW_SHOW);
+}
+
+SEXP do_shellexec(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    SEXP file;
+
+    checkArity(op, args);
+    file = CAR(args);
+    if (!isString(file) || length(file) != 1)
+	errorcall(call, "invalid file argument");
+    internal_shellexec(CHAR(STRING_ELT(file, 0)));
+    return R_NilValue;
+}
+
+int check_doc_file(char * file)
+{
+    char *home, path[MAX_PATH];
+    struct stat sb;
+
+    home = getenv("R_HOME");
+    if (home == NULL)
+	error("R_HOME not set");
+    strcpy(path, home);
+    strcat(path, "/");
+    strcat(path, file);
+    return stat(path, &sb) == 0;
+}
+
+SEXP do_windialog(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    SEXP message, ans;
+    char * type;
+    int res=YES;
+
+    checkArity(op, args);
+    type = CHAR(STRING_ELT(CAR(args), 0));
+    message = CADR(args);
+    if (strcmp(type, "ok")  == 0) {
+	askok(CHAR(STRING_ELT(message, 0)));
+	res = 10;
+    } else if (strcmp(type, "okcancel")  == 0) {
+	res = askokcancel(CHAR(STRING_ELT(message, 0)));
+	if(res == YES) res = 2;
+    } else if (strcmp(type, "yesno")  == 0) {
+	res = askyesno(CHAR(STRING_ELT(message, 0)));
+    } else if (strcmp(type, "yesnocancel")  == 0) {
+	res = askyesnocancel(CHAR(STRING_ELT(message, 0)));
+    } else
+	errorcall(call, "unknown type");
+    ans = allocVector(INTSXP, 1);
+    INTEGER(ans)[0] = res;
+    return (ans);
+}
+
+SEXP do_windialogstring(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    SEXP  message, def, ans;
+    char *string;
+
+    checkArity(op, args);
+    message = CAR(args);
+    def = CADR(args);
+    string = askstring(CHAR(STRING_ELT(message, 0)), CHAR(STRING_ELT(def, 0)));
+    if (string) {
+	ans = allocVector(STRSXP, 1);
+	SET_STRING_ELT(ans, 0, mkChar(string));
+	return (ans);
+    } else
+	return (R_NilValue);
+}
+
+#include "Startup.h"
+extern UImode CharacterMode;
+static char msgbuf[256];
+
+SEXP do_winmenuadd(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    SEXP smenu, sitem;
+    int res;
+    char errmsg[50];
+    
+    checkArity(op, args);
+    if (CharacterMode != RGui)
+	errorcall(call, "Menu functions can only be used in the GUI");
+    smenu = CAR(args);
+    sitem = CADR(args);
+    if (isNull(sitem)) { /* add a menu */
+	res = winaddmenu (CHAR(STRING_ELT(smenu, 0)), errmsg);
+	if (res > 0) {
+	    sprintf(msgbuf, "unable to add menu (%s)", errmsg);
+	    errorcall(call, msgbuf);
+	}
+	
+    } else { /* add an item */
+	res = winaddmenuitem (CHAR(STRING_ELT(sitem, 0)),
+			      CHAR(STRING_ELT(smenu, 0)),
+			      CHAR(STRING_ELT(CADDR(args), 0)),
+			      errmsg);
+	if (res > 0) {
+	    sprintf(msgbuf, "unable to add menu item (%s)", errmsg);
+	    errorcall(call, msgbuf);
+	}
+    }
+    return (R_NilValue);
+}
+
+SEXP do_winmenudel(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    SEXP smenu, sitem;
+    int res;
+    char errmsg[50];
+    
+    checkArity(op, args);
+    if (CharacterMode != RGui)
+	errorcall(call, "Menu functions can only be used in the GUI");
+    smenu = CAR(args);
+    sitem = CADR(args);
+    if (isNull(sitem)) { /* delete a menu */
+	res = windelmenu (CHAR(STRING_ELT(smenu, 0)), errmsg);
+	if (res > 0) 
+	    errorcall(call, "menu does not exist");
+    } else { /* delete an item */
+	res = windelmenuitem (CHAR(STRING_ELT(sitem, 0)),
+			      CHAR(STRING_ELT(smenu, 0)), errmsg);
+	if (res > 0) {
+	    sprintf(msgbuf, "unable to delete menu item (%s)", errmsg);
+	    errorcall(call, msgbuf);
+	}
+    }
+    return (R_NilValue);
+}
+
+
+void Rwin_fpset()
+{
+    _fpreset();
+    _controlfp(_MCW_EM, _MCW_EM);    
+}
+
+#include "getline/getline.h"  /* for gl_load/savehistory */
+SEXP do_savehistory(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    SEXP sfile;
+    
+    checkArity(op, args);
+    sfile = CAR(args);
+    if (!isString(sfile) || LENGTH(sfile) < 1)
+	errorcall(call, "invalid file argument");
+    if (CharacterMode == RGui || (R_Interactive && CharacterMode == RTerm))
+	gl_savehistory(CHAR(STRING_ELT(sfile, 0)));
+    else
+	errorcall(call, "savehistory can only be used in Rgui and Rterm");
+    return R_NilValue;
+}
+
+SEXP do_loadhistory(SEXP call, SEXP op, SEXP args, SEXP env)
+{
+    SEXP sfile;
+    
+    checkArity(op, args);
+    sfile = CAR(args);
+    if (!isString(sfile) || LENGTH(sfile) < 1)
+	errorcall(call, "invalid file argument");
+    if (CharacterMode == RGui || (R_Interactive && CharacterMode == RTerm))
+	gl_loadhistory(CHAR(STRING_ELT(sfile, 0)));
+    else
+	errorcall(call, "savehistory can only be used in Rgui and Rterm");
+    return R_NilValue;
+}
+
+#include <lmcons.h>
+
+SEXP do_sysinfo(SEXP call, SEXP op, SEXP args, SEXP rho)
+{
+    SEXP ans, ansnames;
+    OSVERSIONINFO verinfo;
+    char isNT[8]="??", ver[256], 
+	name[MAX_COMPUTERNAME_LENGTH + 1], user[UNLEN+1];
+    DWORD namelen = MAX_COMPUTERNAME_LENGTH + 1, userlen = UNLEN+1;
+
+    checkArity(op, args);
+    PROTECT(ans = allocVector(STRSXP, 7));
+    verinfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+    GetVersionEx(&verinfo);
+    switch(verinfo.dwPlatformId) {
+    case VER_PLATFORM_WIN32_NT:
+	strcpy(isNT, "NT");
+	break;
+    case VER_PLATFORM_WIN32_WINDOWS:
+	strcpy(isNT, "9x");
+	break;
+    case VER_PLATFORM_WIN32s:
+	strcpy(isNT, "win32s");
+	break;
+    default:
+	sprintf(isNT, "ID=%d", (int)verinfo.dwPlatformId);
+	break;
+    }
+
+    SET_STRING_ELT(ans, 0, mkChar("Windows"));
+    sprintf(ver, "%s %d.%d", isNT,
+	    (int)verinfo.dwMajorVersion, (int)verinfo.dwMinorVersion);
+    SET_STRING_ELT(ans, 1, mkChar(ver));
+    sprintf(ver, "(build %d) %s", LOWORD(verinfo.dwBuildNumber), 
+	    verinfo.szCSDVersion);
+    SET_STRING_ELT(ans, 2, mkChar(ver));
+    GetComputerName(name, &namelen);
+    SET_STRING_ELT(ans, 3, mkChar(name));
+    SET_STRING_ELT(ans, 4, mkChar("x86"));
+    GetUserName(user, &userlen);
+    SET_STRING_ELT(ans, 5, mkChar(user));
+    SET_STRING_ELT(ans, 6, STRING_ELT(ans, 5));
+    PROTECT(ansnames = allocVector(STRSXP, 7));
+    SET_STRING_ELT(ansnames, 0, mkChar("sysname"));
+    SET_STRING_ELT(ansnames, 1, mkChar("release"));
+    SET_STRING_ELT(ansnames, 2, mkChar("version"));
+    SET_STRING_ELT(ansnames, 3, mkChar("nodename"));
+    SET_STRING_ELT(ansnames, 4, mkChar("machine"));
+    SET_STRING_ELT(ansnames, 5, mkChar("login"));
+    SET_STRING_ELT(ansnames, 6, mkChar("user"));
+    setAttrib(ans, R_NamesSymbol, ansnames);
+    UNPROTECT(2);
+    return ans;
+}
+
+void R_ProcessEvents(void); /* from system.c */
+
+SEXP do_syssleep(SEXP call, SEXP op, SEXP args, SEXP rho)
+{
+    DWORD mtime;
+    int ntime;
+    double time;
+    
+    checkArity(op, args);
+    time = asReal(CAR(args));
+    if (ISNAN(time) || time < 0)
+	errorcall(call, "invalid time value");
+    ntime = 1000*(time) + 0.5;
+    while (ntime > 0) {
+	mtime = min(500, ntime);
+	ntime -= mtime;
+	Sleep(mtime);
+	R_ProcessEvents();
+    }
+    return R_NilValue;
+}
---
+++ tags/R-GenGC-merged-08-09-2000/src/gnuwin32/front-ends/SC_proxy.h
@@ -0,0 +1,260 @@
+/*
+ *  StatConn: Connector interface between application and interpreter language
+ *  Copyright (C) 1999 Thomas Baier
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ *  MA 02111-1307, USA
+ *
+ *  $Id: SC_proxy.h,v 1.3 1999/12/11 20:57:35 pd Exp $
+ */
+
+#ifndef _STATCONN_H_
+#define _STATCONN_H_
+
+#include "bdx.h"
+
+// system-specifics should be moved to some include file
+#include <windows.h>
+#define SYSCALL WINAPI
+#define EXPORT
+
+// forward definition
+struct _SC_Proxy_Object;
+struct _SC_CharacterDevice;
+
+// error codes
+
+// generic error and success codes
+#define SC_PROXY_OK                            0x00000000
+#define SC_PROXY_ERR_UNKNOWN                   0x80000000
+
+#define SC_PROXY_ERR_INVALIDARG                0x80000001
+#define SC_PROXY_ERR_INVALIDFORMAT             0x80000002
+#define SC_PROXY_ERR_NOTIMPL                   0x80000003
+
+// initialization and termination
+#define SC_PROXY_ERR_INITIALIZED               0x80000004
+#define SC_PROXY_ERR_NOTINITIALIZED            0x80000005
+
+// evaluation, getting and setting symbols
+#define SC_PROXY_ERR_INVALIDSYMBOL             0x80000006
+#define SC_PROXY_ERR_PARSE_INVALID             0x80000007
+#define SC_PROXY_ERR_PARSE_INCOMPLETE          0x80000008
+#define SC_PROXY_ERR_UNSUPPORTEDTYPE           0x80000009
+
+// version mismatch
+#define SC_PROXY_ERR_INVALIDINTERFACEVERSION   0x80000010
+#define SC_PROXY_ERR_INVALIDINTERPRETERVERSION 0x80000011
+
+// type mask values
+#define SC_TM_SCALAR_BOOL   (BDX_BOOL)
+#define SC_TM_SCALAR_INT    (BDX_INT)
+#define SC_TM_SCALAR_DOUBLE (BDX_DOUBLE)
+#define SC_TM_SCALAR_STRING (BDX_STRING)
+#define SC_TM_SCALAR_ALL    (SC_TM_SCALAR_BOOL     \
+                             | SC_TM_SCALAR_INT    \
+                             | SC_TM_SCALAR_DOUBLE \
+                             | SC_TM_SCALAR_STRING)
+
+#define SC_TM_ARRAY_BOOL    (BDX_BOOL << 8)
+#define SC_TM_ARRAY_INT     (BDX_INT << 8)
+#define SC_TM_ARRAY_DOUBLE  (BDX_DOUBLE << 8)
+#define SC_TM_ARRAY_STRING  (BDX_STRING << 8)
+#define SC_TM_ARRAY_ALL     (SC_TM_ARRAY_BOOL     \
+                             | SC_TM_ARRAY_INT    \
+                             | SC_TM_ARRAY_DOUBLE \
+                             | SC_TM_ARRAY_STRING)
+
+#define SC_TM_VECTOR_BOOL   (BDX_BOOL << 12)
+#define SC_TM_VECTOR_INT    (BDX_INT << 12)
+#define SC_TM_VECTOR_DOUBLE (BDX_DOUBLE << 12)
+#define SC_TM_VECTOR_STRING (BDX_STRING << 12)
+#define SC_TM_VECTOR_ALL    (SC_TM_VECTOR_BOOL     \
+                             | SC_TM_VECTOR_INT    \
+                             | SC_TM_VECTOR_DOUBLE \
+                             | SC_TM_VECTOR_STRING)
+
+// information main keys
+#define SC_INFO_MAIN_CONNECTOR     1
+#define SC_INFO_MAIN_INTERPRETER   2
+
+// information sub keys
+#define SC_INFO_SUB_NAME                  1
+#define SC_INFO_SUB_DESCRIPTION           2
+#define SC_INFO_SUB_COPYRIGHT             3
+#define SC_INFO_SUB_LICENSE               4
+#define SC_INFO_SUB_MINORVERSION          5
+#define SC_INFO_SUB_MAJORVERSION          6
+
+// function type-defs
+typedef int (SYSCALL *SC_PROXY_GET_VERSION) (struct _SC_Proxy_Object* object,
+					     unsigned long* version);
+typedef int (SYSCALL *SC_PROXY_INIT) (struct _SC_Proxy_Object* object);
+typedef int (SYSCALL *SC_PROXY_TERMINATE) (struct _SC_Proxy_Object* object);
+typedef int (SYSCALL *SC_PROXY_RETAIN) (struct _SC_Proxy_Object* object);
+typedef int (SYSCALL *SC_PROXY_RELEASE) (struct _SC_Proxy_Object* object);
+
+typedef int (SYSCALL *SC_PROXY_SET_SYMBOL) (struct _SC_Proxy_Object* object,
+					    char const* symbol,
+					    BDX_Data /* const */ * data);
+typedef int (SYSCALL *SC_PROXY_GET_SYMBOL) (struct _SC_Proxy_Object* object,
+					    char const* symbol,
+					    BDX_Data** data);
+typedef int (SYSCALL *SC_PROXY_EVALUATE) (struct _SC_Proxy_Object* object,
+					  char const* command,
+					  BDX_Data** data);
+typedef int (SYSCALL *SC_PROXY_EVALUATE_NORETURN) (struct _SC_Proxy_Object* object,
+						   char const* command);
+typedef int (SYSCALL *SC_PROXY_FREE_DATA_BUFFER) (struct _SC_Proxy_Object* object,
+						  BDX_Data* data);
+typedef int (SYSCALL *SC_PROXY_QUERY_TYPES) (struct _SC_Proxy_Object* object,
+					     long* type_mask);
+typedef int (SYSCALL *SC_PROXY_QUERY_OPS) (struct _SC_Proxy_Object* object,
+					   long* op_mask);
+typedef int (SYSCALL *SC_PROXY_SET_CHARACTERDEVICE)
+(
+  struct _SC_Proxy_Object* object,
+  struct _SC_CharacterDevice* device
+);
+typedef int (SYSCALL *SC_PROXY_QUERY_INFO) (struct _SC_Proxy_Object* object,
+					    long main_key,
+					    long sub_key,
+					    const char** information);
+
+// character device function typedefs
+typedef int (SYSCALL *SC_CHARACTERDEVICE_GET_VERSION)
+(
+  struct _SC_CharacterDevice* object,
+  unsigned long* version
+);
+
+typedef int (SYSCALL *SC_CHARACTERDEVICE_RETAIN)
+(
+  struct _SC_CharacterDevice* object
+);
+
+typedef int (SYSCALL *SC_CHARACTERDEVICE_RELEASE)
+(
+  struct _SC_CharacterDevice* object
+);
+
+typedef int (SYSCALL *SC_CHARACTERDEVICE_WRITE_STRING)
+(
+  struct _SC_CharacterDevice* object,
+  char const* string
+);
+
+typedef int (SYSCALL *SC_CHARACTERDEVICE_WRITE_STRING_LEVEL)
+(
+  struct _SC_CharacterDevice* object,
+  char const* string,
+  unsigned long level
+);
+
+/*
+ * interface version information:
+ *
+ * 1 ... obsolete first interface
+ * 2 ... get_version, set_symbol, evaluate_noreturn, evaluate
+ * 3 ... added character devices for output, errors and tracing, info strings
+ */
+#define SC_PROXY_INTERFACE_VERSION 3
+#define SC_CHARACTERDEVICE_VERSION 1
+
+// character device used for output, (textual) error messages and traces
+typedef struct _SC_CharacterDevice_Vtbl
+{
+  // get character device version
+  SC_CHARACTERDEVICE_GET_VERSION get_version;
+
+  // increase the reference count to this interface object
+  SC_CHARACTERDEVICE_RETAIN retain;
+  // decrease the reference count to this interface object, object will be
+  // freed when count reaches 0
+  SC_CHARACTERDEVICE_RELEASE release;
+
+  // write a string to the output device
+  SC_CHARACTERDEVICE_WRITE_STRING write_string;
+
+  // write a string to the output device. the string is echoed if the passed
+  // level is less or equal to the current output level of the device (used
+  // for conditional output)
+  SC_CHARACTERDEVICE_WRITE_STRING_LEVEL write_string_level;
+} SC_CharacterDevice_Vtbl;
+
+
+// used for communication between the COM/CORBA server and R
+typedef struct _SC_Proxy_Object_Vtbl
+{
+  // get interpreter version
+  SC_PROXY_GET_VERSION get_version;
+
+  // initialize the interpreter
+  SC_PROXY_INIT init;
+  // terminate the interpreter
+  SC_PROXY_TERMINATE terminate;
+
+  // increase the reference count to this interface object
+  SC_PROXY_RETAIN retain;
+  // decrease the reference count to this interface object, object will be
+  // freed when count reaches 0
+  SC_PROXY_RELEASE release;
+
+  // set a symbol in the interpreter's global namespace to the BDX data passed
+  SC_PROXY_SET_SYMBOL set_symbol;
+  // return a symbol's value from the interpreter's global namespace
+  SC_PROXY_GET_SYMBOL get_symbol;
+
+  // evaluate an expression, return result in a BDX data buffer (synchronously)
+  SC_PROXY_EVALUATE evaluate;
+  // evaluate an expression, do not return a result (synchronously)
+  SC_PROXY_EVALUATE_NORETURN evaluate_noreturn;
+
+  // return information about BDX data types supported by this interface and
+  // the interpreter
+  SC_PROXY_QUERY_TYPES query_supported_types;
+  // return information about available functionality in this interface and
+  // the interpreter
+  SC_PROXY_QUERY_OPS query_supported_operations;
+
+  // free a BDX data buffer allocated by one of this interface's functions
+  SC_PROXY_FREE_DATA_BUFFER free_data_buffer;
+
+  // set output, error and tracing devices
+  SC_PROXY_SET_CHARACTERDEVICE set_output_device;
+
+  // retrieve information about interface and interpreter
+  SC_PROXY_QUERY_INFO query_info;
+
+} SC_Proxy_Object_Vtbl;
+
+// abstract data type (implementation adds data)
+typedef struct _SC_Proxy_Object
+{
+  SC_Proxy_Object_Vtbl* vtbl;
+} SC_Proxy_Object;
+
+typedef struct _SC_CharacterDevice
+{
+  SC_CharacterDevice_Vtbl* vtbl;
+} SC_CharacterDevice;
+
+// entry point: retrieve a proxy object with a given version
+typedef int (SYSCALL* SC_PROXY_GET_OBJECT) (SC_Proxy_Object**,unsigned long);
+
+// this is valid for Windows only
+#define SC_PROXY_GET_OBJECT_FUN "SC_Proxy_get_object@8"
+
+#endif
