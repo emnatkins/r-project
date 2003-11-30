a504fffb3ae399a0419e03bedaa5b6d7eb522dfb
nobody <nobody@00db46b3-68df-0310-9c12-caf00c1e9a41>
2003-11-30 16:40:30 +0000

    This commit was manufactured by cvs2svn to create tag 'R-grid-update'.

---
+++ tags/R-grid-update/src/library/base/R/array.R
@@ -0,0 +1,37 @@
+array <-
+function(data = NA, dim = length(data), dimnames = NULL)
+{
+    data <- as.vector(data)
+    vl <- prod(dim)
+    if( length(data) != vl  ) {
+	t1 <- ceiling(vl/length(data))
+	data <- rep.int(data,t1)
+	if( length(data) != vl )
+	    data <- data[1:vl]
+    }
+    if(length(dim))
+	dim(data) <- dim
+    if(is.list(dimnames) && length(dimnames))
+	dimnames(data) <- dimnames
+    data
+}
+
+slice.index <-
+function(x, MARGIN)
+{
+    d <- dim(x)
+    if(is.null(d))
+        d <- length(x)
+    n <- length(d)
+
+    if((length(MARGIN) > 1) || (MARGIN < 1) || (MARGIN > n))
+        stop("incorrect value for MARGIN")
+
+    if(any(d == 0)) return(array(integer(0), d))
+
+    y <- rep.int(rep.int(seq(1 : d[MARGIN]),
+                 prod(d[seq(length = MARGIN - 1)]) * rep.int(1, d[MARGIN])),
+             prod(d[seq(from = MARGIN + 1, length = n - MARGIN)]))
+    dim(y) <- d
+    y
+}
