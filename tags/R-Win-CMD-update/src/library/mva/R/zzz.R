d1e220a294589c35a2ec3a0f6843bddd2634c395
nobody <nobody@00db46b3-68df-0310-9c12-caf00c1e9a41>
2004-02-20 12:40:06 +0000

    This commit was manufactured by cvs2svn to create tag 'R-Win-CMD-update'.

---
+++ tags/R-Win-CMD-update/src/library/mva/R/zzz.R
@@ -0,0 +1,10 @@
+.noGenerics <- TRUE
+.conflicts.OK <- TRUE
+
+.onLoad <- .First.lib <- function(lib, pkg)
+{
+    have.stats <- "package:stats" %in% search()
+    if(!have.stats) require("stats")
+    warning("package ", sQuote("mva"), " has been merged into ",
+            sQuote("stats"), call. = FALSE)
+}
