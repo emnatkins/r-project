6e0cf0831c0abecfc23c907427c60ce96bc154c8
murdoch <murdoch@00db46b3-68df-0310-9c12-caf00c1e9a41>
2006-12-14 17:37:33 +0000

    Remove temporary branch

--- branches/djm-source/src/gnuwin32/fixed/bin/Stangle.sh
+++
@@ -1,3 +0,0 @@
-R_EXE="${R_HOME}/bin/rterm.exe"
-echo "library(\"utils\"); Stangle(\"$1\")" | \
-  "${R_EXE}" --no-restore --slave
--- branches/djm-source/src/gnuwin32/fixed/bin/Sweave.sh
+++
@@ -1,3 +0,0 @@
-R_EXE="${R_HOME}/bin/rterm.exe"
-echo "library(\"utils\"); Sweave(\"$1\")" | \
-  "${R_EXE}" --no-restore --slave
