f5e91e3a1a7627559101fda4e2d2b4032185c328
pd <pd@00db46b3-68df-0310-9c12-caf00c1e9a41>
2012-06-22 07:09:44 +0000

    Tag version 2.15.1

---
+++ tags/R-2-15-1/src/library/tools/R/xgettext.R
@@ -0,0 +1,185 @@
+#  File src/library/tools/R/xgettext.R
+#  Part of the R package, http://www.R-project.org
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
+xgettext <-
+function(dir, verbose = FALSE, asCall = TRUE)
+{
+    dir <- file_path_as_absolute(dir)
+    bn <- basename(dir)
+    dir <- file.path(dir, "R")
+    exts <- .make_file_exts("code")
+    R_files <- list_files_with_exts(dir, exts)
+    for(d in c("unix", "windows")) {
+        OSdir <- file.path(dir, d)
+        if(file_test("-d", OSdir))
+            R_files <- c(R_files, list_files_with_exts(OSdir, exts))
+    }
+    if(bn == "base") {
+        ## include loader files in R_HOME/share/R
+        shdir <- file.path(dir, "../../../../share/R")
+        R_files <- c(R_files, list_files_with_exts(shdir, exts))
+    }
+    out <- vector("list", length = length(R_files))
+    names(out) <- R_files
+
+    find_strings <- function(e) {
+        find_strings2 <- function(e, suppress) {
+            if(is.character(e)) {
+                if(!suppress) strings <<- c(strings, e)
+            } else if(is.call(e)) {
+                if(is.name(e[[1L]])
+                   && (as.character(e[[1L]])
+                       %in% c("gettext", "gettextf"))) {
+                    domain <- e[["domain"]]
+                    suppress <- !is.null(domain) && !is.name(domain) && is.na(domain)
+                    if(as.character(e[[1L]]) == "gettextf") {	
+                        e <- match.call(gettextf, e)
+                        e <- e["fmt"] # just look at fmt arg
+                    } else if(as.character(e[[1L]]) == "gettext" &&
+                              !is.null(names(e))) {
+                        e <- e[!(names(e) == "domain")] # remove domain arg
+                    }
+                }
+                for(i in seq_along(e)) find_strings2(e[[i]], suppress)
+            }
+        }
+        if(is.call(e)
+           && is.name(e[[1L]])
+           && (as.character(e[[1L]])
+               %in% c("warning", "stop", "message", "packageStartupMessage",
+                      "gettext", "gettextf"))) {
+             domain <- e[["domain"]]
+             suppress <- !is.null(domain) && !is.name(domain) && is.na(domain)
+             ## remove named args
+             if(!is.null(names(e)))
+                 e <- e[!names(e) %in% c("call.", "immediate.", "domain")]
+             if(asCall) {
+                 if(!suppress) strings <<- c(strings, as.character(e)[-1L])
+             } else for(i in seq_along(e)) find_strings2(e[[i]], suppress)
+        } else if(is.recursive(e))
+            for(i in seq_along(e)) Recall(e[[i]])
+    }
+
+    for(f in R_files) {
+        if(verbose) message(gettextf("parsing '%s'", f), domain = NA)
+        strings <- character()
+        for(e in parse(file = f)) find_strings(e)
+        ## strip leading and trailing white space
+        strings <- sub("^[ \t\n]*", "", strings)
+        strings <- sub("[ \t\n]*$", "", strings)
+        out[[f]] <- structure(unique(strings), class="xgettext")
+    }
+
+    out[sapply(out, length) > 0L]
+}
+
+print.xgettext <-
+function(x, ...)
+{
+    cat(x, sep="\n")
+    invisible(x)
+}
+
+print.xngettext <-
+function(x, ...)
+{
+    lapply(x, function(x)
+           cat("\nmsgid        = ", x[1L],
+               "\nmsgid_plural = ", x[2L],
+               "\n", sep=""))
+    invisible(x)
+}
+
+xngettext <-
+function(dir, verbose = FALSE)
+{
+    dir <- file_path_as_absolute(dir)
+    dir <- file.path(dir, "R")
+    exts <- .make_file_exts("code")
+    R_files <- list_files_with_exts(dir, exts)
+    for(d in c("unix", "windows", "aqua")) {
+        OSdir <- file.path(dir, d)
+        if(file_test("-d", OSdir))
+            R_files <- c(R_files, list_files_with_exts(OSdir, exts))
+    }
+    out <- vector("list", length = length(R_files))
+    names(out) <- R_files
+
+    find_strings <- function(e) {
+        if(is.call(e) && is.name(e[[1L]])
+           && as.character(e[[1L]]) %in% "ngettext") {
+	    e <- match.call(ngettext, e)
+	    if (is.character(e[["msg1"]]) && is.character(e[["msg2"]]))
+	    	strings <<- c(strings, list(c(msg1=e[["msg1"]], 
+	    				      msg2=e[["msg2"]])))
+        } else if(is.recursive(e))
+            for(i in seq_along(e)) Recall(e[[i]])
+    }
+
+    for(f in R_files) {
+        if(verbose) message(gettextf("parsing '%s'", f), domain = NA)
+        strings <- list()
+        for(e in parse(file = f)) find_strings(e)
+        out[[f]] <- structure(strings, class="xngettext")
+    }
+
+    out[sapply(out, length) > 0L]
+}
+
+xgettext2pot <-
+function(dir, potFile)
+{
+    dir <- file_path_as_absolute(dir)
+    if(missing(potFile))
+        potFile <- paste0("R-", basename(dir), ".pot")
+    tmp <- unique(unlist(xgettext(dir, asCall = FALSE)))
+    tmp <- tmp[nzchar(tmp)]
+    if(length(tmp) > 0L)
+	tmp <- shQuote(encodeString(tmp), type="cmd")  # need to quote \n, \t etc
+    con <- file(potFile, "wt")
+    on.exit(close(con))
+    writeLines(con=con,
+               c('msgid ""',
+                 'msgstr ""',
+                 sprintf('"Project-Id-Version: R %s.%s\\n"',
+                         R.version$major, R.version$minor),
+                 '"Report-Msgid-Bugs-To: bugs@r-project.org\\n"',
+                 paste0('"POT-Creation-Date: ',
+                        format(Sys.time(), "%Y-%m-%d %H:%M"), # %z is not portable
+                        '\\n"'),
+                 '"PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\\n"',
+                 '"Last-Translator: FULL NAME <EMAIL@ADDRESS>\\n"',
+                 '"Language-Team: LANGUAGE <LL@li.org>\\n"',
+                 '"MIME-Version: 1.0\\n"',
+                 '"Content-Type: text/plain; charset=CHARSET\\n"',
+                 '"Content-Transfer-Encoding: 8bit\\n"', ''))
+    for(e in tmp)
+        writeLines(con=con, c('', paste('msgid', e), 'msgstr ""'))
+    tmp <- xngettext(dir)
+    un <- unique(unlist(tmp, recursive=TRUE))
+    for(ee in tmp)
+        for(e in ee)
+            if(e[1L] %in% un) {
+                writeLines(con=con, c('',
+                           paste('msgid       ',
+                                 shQuote(encodeString(e[1L]), type="cmd")),
+                           paste('msgid_plural',
+                                 shQuote(encodeString(e[2L]), type="cmd")),
+                           'msgstr[0]    ""', 'msgstr[1]    ""')
+                           )
+                un <- un[-match(e, un)]
+            }
+}
